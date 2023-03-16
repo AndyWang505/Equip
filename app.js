ea4300f5024f40241b15ab9a71e422b5310da9d0
andywang505 <andywang890505@gmail.com>
2023-03-16 16:34:24 +0800

    feat: add delete metaInfo some data

--- app.js
+++ app.js
@@ -86,6 +86,10 @@ async function findIdItem(){
             delete rest.metaInfo.icon;
             delete rest.metaInfo.vslots;
             delete rest.metaInfo.islots;
+            delete rest.metaInfo.charmEXP;
+            delete rest.metaInfo.tradeAvailable;
+            delete rest.metaInfo.setCompleteCount;
+            delete rest.metaInfo.bossReward;
             return { ...rest };
         });
         console.log('update...');
